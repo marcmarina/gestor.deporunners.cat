ea10ef99ccf646217098fec3239fc0a7bdd5e378
Marc Marina Miravitlles <marc.marina.miravitlles@gmail.com>
2020-08-12 20:08:19 +0200

    Login WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import LoginScreen from './components/LoginScreen';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route path="/login" component={LoginScreen} />
+        <Redirect from="/" to="/login" />
+      </Switch>
+    </>
   );
 }
 
