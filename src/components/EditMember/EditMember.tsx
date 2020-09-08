530065628cd404cb7dac50dcf03879637770e5f8
Marc Marina Miravitlles <marc.marina.miravitlles@gmail.com>
2020-09-08 16:31:12 +0200

    Edit member WIP

--- src/components/EditMember/EditMember.tsx
+++ src/components/EditMember/EditMember.tsx
@@ -1,12 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import { Paper } from '@material-ui/core';
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+} from '@material-ui/core';
+import { useParams } from 'react-router-dom';
+import { fetchById } from '../../services/member';
+import axios from 'axios';
+
+type TParams = {
+  id: string;
+};
+
+interface Member {
+  firstName: string;
+  lastName: string;
+  numMember: number;
+  email: string;
+  dni: string;
+  telephone: string;
+  address: {
+    postCode: string;
+    streetAddress: string;
+    town: string;
+  };
+}
 
 export default function EditMember() {
+  const handleChange = (e: any) => {
+    setTown(e.target.value);
+  };
+
+  const [member, setMember] = useState<Member>();
+  const [towns, setTowns] = useState<any>([]);
+
+  const [firstName, setFirstName] = useState<string>();
+  const [lastName, setLastName] = useState<string>();
+
+  const [town, setTown] = useState('');
+
+  const { id } = useParams<TParams>();
+
+  const retrieveMember = async (id: string) => {
+    try {
+      const { data } = await fetchById(id);
+      setMember(data);
+      setTown(data.address.town);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
+  const retrieveTowns = async () => {
+    try {
+      const { data } = await axios.get('/api/town');
+      setTowns(data);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
+  useEffect(() => {
+    retrieveTowns();
+    retrieveMember(id);
+  }, []);
+
+  if (!member || !towns) return null;
+
   return (
     <Paper
       style={{
@@ -23,6 +90,7 @@ export default function EditMember() {
             id="firstName"
             name="firstName"
             label="Nom"
+            value={member.firstName}
             fullWidth
           />
         </Grid>
@@ -33,6 +101,7 @@ export default function EditMember() {
             name="lastName"
             label="Cognoms"
             fullWidth
+            value={member.lastName}
             autoComplete="family-name"
           />
         </Grid>
@@ -43,35 +112,27 @@ export default function EditMember() {
             name="address1"
             label="Address line 1"
             fullWidth
+            value={member.address.streetAddress}
             autoComplete="shipping address-line1"
           />
         </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="address2"
-            name="address2"
-            label="Address line 2"
-            fullWidth
-            autoComplete="shipping address-line2"
-          />
-        </Grid>
         <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="shipping address-level2"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="state"
-            name="state"
-            label="State/Province/Region"
-            fullWidth
-          />
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Ciutat</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              onChange={handleChange}
+              value={town}
+            >
+              {towns &&
+                towns.map((town: any) => (
+                  <MenuItem key={town._id} value={town._id}>
+                    {town.name}
+                  </MenuItem>
+                ))}
+            </Select>
+          </FormControl>
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
