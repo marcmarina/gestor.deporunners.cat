60020cbfa6362f58286222b13044211a3be0a9c1
Marc Marina <marc.marina.miravitlles@gmail.com>
2023-01-21 11:47:09 +0000

    Added api token from environment

--- src/config/production.ts
+++ src/config/production.ts
@@ -2,6 +2,6 @@ import { Config } from './config';
 
 export const production: Config = {
   API_URL: 'https://deporunners-api.onrender.com',
-  API_TOKEN: 'TODO',
+  API_TOKEN: import.meta.env.VITE_API_TOKEN,
   STRIPE_PUB_KEY: 'pk_live_Mr7IflqsHTvia79JXncob2C3',
 };
--- src/config/staging.ts
+++ src/config/staging.ts
@@ -2,7 +2,7 @@ import { Config } from './config';
 
 export const staging: Config = {
   API_URL: 'https://deporunners-api-staging.onrender.com',
-  API_TOKEN: 'TODO',
+  API_TOKEN: import.meta.env.VITE_API_TOKEN,
   STRIPE_PUB_KEY:
     'pk_test_51D84cOEHf0A3zmt8dw5uwY9cSgwBAyL0ltom8TTIEMK30xkxbjQ5xf8ppwVaMxdiDqzehqvLIwEfmrcMuMfdGqTO00Vv6IfY9H',
 };
